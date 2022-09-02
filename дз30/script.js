f33cdd4de9e9e3377d96b94f5ea5e8bc2e6bac19
BrainHD <miks.max@mail.ru>
2022-09-02 16:33:22 +0300

    new changes dz30

--- дз30/script.js
+++ дз30/script.js
@@ -4,34 +4,45 @@ const images = document.querySelectorAll('img');
 
 for (let image of images) {
 
+   
    image.addEventListener('mousedown', mouseDown);
+   image.addEventListener('load', getCoords);
    image.addEventListener('mouseenter', mouseEnter);
+   
+
+   function getCoords(e) {
+      e = e || window.event;
+      let self = this;
+      self.style.position = 'absolute';
+      self.style.left = Math.ceil(self.getBoundingClientRect().left) + 'px';
+      self.style.top = Math.ceil(self.getBoundingClientRect().top) + 'px'; 
+   }
    
    function mouseDown(e) {
       e = e || window.event;
       let self = this;
       
+      self.style.position = 'absolute';
       let shiftX = e.clientX - self.getBoundingClientRect().left; 
       let shiftY = e.clientY - self.getBoundingClientRect().top;
-   
-      self.style.position = 'absolute';
+      
       self.style.cursor = 'grabbing';
-      self.style.zIndex = 10;
+      
       
       document.addEventListener('mousemove', mouseMove);
       
       function mouseMove(e) {
          e = e || window.event;
          
+         self.style.zIndex = 10;
          image.style.left = e.pageX - shiftX + 'px';
          image.style.top = e.pageY - shiftY + 'px';
       }
-   
+      
       image.addEventListener('mouseup', mouseUp);
       
       function mouseUp(e) {
          e = e || window.event;
-         
          document.removeEventListener('mousemove', mouseMove);
          e.target.style.cursor = 'grab';
       }
