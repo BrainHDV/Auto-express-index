fb8842bfd9b05cb012e169a7b2116dbec9a877ef
BrainHD <miks.max@mail.ru>
2022-10-17 11:46:12 +0300

    comm

---
+++ дз37/js/supportFunc.js
@@ -0,0 +1,13 @@
+function addError(elem, selector) {
+  elem.classList.add("error");
+  selector.style.display = "block";
+}
+
+function removeError(elem, selector) {
+  elem.classList.remove("error");
+  selector.style.display = "none";
+}
+
+function addZero(num) {
+  return num < 10 ? "0" + num : num;
+}
