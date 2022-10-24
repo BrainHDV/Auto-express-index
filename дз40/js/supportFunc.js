09f4db2971b2ef29d6316a8bc4679b87e531e227
BrainHD <miks.max@mail.ru>
2022-10-24 17:52:12 +0300

    fix

--- дз40/js/supportFunc.js
+++ дз40/js/supportFunc.js
@@ -1,3 +1,13 @@
 function addZero(num) {
   return num < 10 ? "0" + num : num;
 }
+
+function addError(elem, selector) {
+  elem.classList.add("error");
+  selector.style.display = "block";
+}
+
+function removeError(elem, selector) {
+  elem.classList.remove("error");
+  selector.style.display = "none";
+}
